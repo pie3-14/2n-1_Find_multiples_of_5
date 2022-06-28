8792571f32fa0c32a53738b7eab6ecd962a69bbe
pie3-14 <57256066+pie3-14@users.noreply.github.com>
2022-06-28 20:34:36 +0900

    Add files via upload

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void) {
+	int n = 1;
+	int count = 0;
+	
+	for (int i = 0; i < 10000; i++) {
+		int result = 2 * n - 1;
+		printf("%d\n", result);
+		if (result % 5 == 0) {
+			count++;
+		}
+		n++;
+	}
+	printf("result = %d\n", count);
+	return 0;
+}
